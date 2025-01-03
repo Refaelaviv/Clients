8e76efd772dbc21e6d549011facf95603b350bbd
Refaelaviv <rafushka98@gmail.com>
2025-01-03 22:39:39 +0200

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,12 @@
+
+```javascript
+export default function RootLayout({ children }) {
+  return (
+    <html lang="he" dir="rtl">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
+```
